c2ca249a3e32c131fcf9d76072fa2ebe1e863971
wiktoria-wn <niezboralawiktoria@gmail.com>
2020-08-02 19:05:52 +0200

    Updates

---
+++ precache-manifest.55cdd46ce54e97eeecc69e1bf4be12aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb83e9e767275d9e83a8862eb19b0d3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ac26d81c1adc01bf1d08",
+    "url": "/portfolio/static/js/2.31b4b6b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.31b4b6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252a099784e8accebf44",
+    "url": "/portfolio/static/js/main.5441418b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5b43282890956b08ef6510129ec06358",
+    "url": "/portfolio/static/media/bg.5b432828.jpg"
+  },
+  {
+    "revision": "62946dc7a2a24206bd4540b11ccf1f94",
+    "url": "/portfolio/static/media/github.62946dc7.svg"
+  },
+  {
+    "revision": "9681e71d3ae51a3f92fab0ed74722e49",
+    "url": "/portfolio/static/media/homeimg.9681e71d.svg"
+  },
+  {
+    "revision": "b4b5322e3b6919688dba7bb1edb330fb",
+    "url": "/portfolio/static/media/linkedin.b4b5322e.svg"
+  }
+]);
